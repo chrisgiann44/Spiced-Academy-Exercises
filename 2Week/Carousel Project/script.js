6f33803caa7abb0bd84050de419d903e1cef1a15
Christos Giannouris <chris1giann1@gmail.com>
2019-04-12 16:03:21 +0200

    Carousel

--- 2Week/Carousel Project/script.js
+++ 2Week/Carousel Project/script.js
@@ -1,28 +1,51 @@
 (function() {
     var mtains = document.getElementsByClassName("mtain");
-
+    var dots = document.getElementsByClassName("dot");
     var cur = 0;
+    var transitioning;
+    var timer;
 
-    setTimeout(carouselmv, 3000);
+    timer = setTimeout(carousel, 3000);
 
     document.addEventListener("transitionend", function(e) {
         if (e.target.classList.contains("exit")) {
+            timer = setTimeout(carousel, 3000);
             e.target.classList.remove("exit");
         }
+
+        transitioning = false;
     });
 
-    function carouselmv() {
+    for (var i = 0; i < dots.length; i++) {
+        (function(i) {
+            dots[i].addEventListener("click", function() {
+                if (transitioning) {
+                    return;
+                } else {
+                    clearTimeout(timer);
+                    carousel(i);
+                }
+            });
+        })(i);
+    }
+
+    function carousel(arg) {
         mtains[cur].classList.remove("onscreen");
-
         mtains[cur].classList.add("exit");
+        dots[cur].classList.remove("on");
 
-        cur++;
-        if (cur >= mtains.length) {
-            cur = 0;
+        transitioning = true;
+
+        if (arg >= 0) {
+            cur = arg;
+        } else {
+            cur++;
+            if (cur >= mtains.length) {
+                cur = 0;
+            }
         }
 
         mtains[cur].classList.add("onscreen");
-
-        setTimeout(carouselmv, 3000);
+        dots[cur].classList.add("on");
     }
 })();
