1ebac2f8eddda77e28baa6caf75e13ac694df0ff
Christos Giannouris <chris1giann1@gmail.com>
2019-04-17 14:32:31 +0200

    exers

---
+++ 3Week/3day/exer2/script.js
@@ -0,0 +1,35 @@
+(function() {
+    function askForNumber() {
+        var num = prompt("Please enter a number between 1 and 10");
+        if (num >= 1 && num <= 10 && num == parseInt(num)) {
+            return num;
+        }
+        throw new Error("Bad number");
+    }
+
+    var germNumbers = [
+        "eins",
+        "zwei",
+        "drei",
+        "vier",
+        "fünf",
+        "sechs",
+        "sieben",
+        "acht",
+        "neun",
+        "zehn"
+    ];
+
+    translateNumberToGerman();
+
+    function translateNumberToGerman() {
+        try {
+            var num = askForNumber();
+            console.log(germNumbers[num - 1]);
+            return germNumbers[num - 1];
+        } catch (e) {
+            console.log(e);
+            translateNumberToGerman();
+        }
+    }
+})();
