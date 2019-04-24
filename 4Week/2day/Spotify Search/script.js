6310a31fdd4844eae2ca5553043fbe387db54806
Christos Giannouris <chris1giann1@gmail.com>
2019-04-24 22:25:59 +0200

    Spotify

---
+++ 4Week/2day/Spotify Search/script.js
@@ -0,0 +1,83 @@
+(function() {
+    var nextUrl;
+
+    function ajaxCall(url, data) {
+        console.log(4);
+        $.ajax({
+            url: url,
+            data: data,
+            success: function(data) {
+                data = data.artists || data.albums;
+                nextUrl =
+                    data.next &&
+                    data.next.replace(
+                        "https://api.spotify.com/v1/search",
+                        "https://elegant-croissant.glitch.me/spotify"
+                    );
+
+                if (nextUrl) {
+                    $(".moreResults").css({
+                        visibility: "visible"
+                    });
+                }
+
+                if (!nextUrl) {
+                    $(".moreResults").css({
+                        visibility: "hidden"
+                    });
+                }
+
+                var links = [];
+                var names = [];
+                var images = [];
+                var htmlContent = "";
+
+                for (var i = 0; i < data.items.length; i++) {
+                    links.push(data.items[i].external_urls["spotify"]);
+                    names.push(data.items[i].name);
+                    if (data.items[i].images[0] != null) {
+                        images.push(data.items[i].images[0].url);
+                    } else {
+                        images.push("/icon.jpg");
+                    }
+                }
+
+                for (var z = 0; z < links.length; z++) {
+                    htmlContent +=
+                        '<div class="result">' +
+                        '<img src="' +
+                        images[z] +
+                        '">' +
+                        '<a href="' +
+                        links[z] +
+                        '">' +
+                        names[z] +
+                        "</a></div>";
+                }
+                if ($(".results").html() == null) {
+                    $(".results").html(htmlContent);
+                } else {
+                    $(".results").append(htmlContent);
+                }
+            }
+        });
+    }
+
+    $(".submit").on("click", function() {
+        $(".moreResults").css({
+            visibility: "hidden"
+        });
+
+        var userInput = $("input[name='user-input']").val();
+        var selection = $(".select").val();
+
+        ajaxCall("https://elegant-croissant.glitch.me/spotify", {
+            query: userInput,
+            type: selection
+        });
+    });
+
+    $(".moreResults").on("click", function() {
+        ajaxCall(nextUrl, null);
+    });
+})();
