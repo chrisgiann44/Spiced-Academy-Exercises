3b61bba2ff9bb8eb92034cd58d17e33b686a103c
Christos Giannouris <chris1giann1@gmail.com>
2019-04-30 16:01:13 +0200

    day2

--- 5Week/2day/index.js
+++ 5Week/2day/index.js
@@ -24,4 +24,32 @@ function statFind(url, arg) {
     });
 }
 
-logSize(__dirname + "/" + "fun-with-fs");
+// logSize(__dirname + "/fun-with-fs");
+
+var x = mapSizes(__dirname + "/fun-with-fs/files");
+
+function mapSizes(url) {
+    let obj = {};
+
+    let path = url;
+
+    var currentFiles = fs.readdirSync(path, { withFileTypes: true });
+
+    for (var i = 0; i < currentFiles.length; i++) {
+        if (currentFiles[i].isFile()) {
+            obj[currentFiles[i].name] = fs.statSync(
+                path + "/" + currentFiles[i].name
+            ).size;
+        } else if (currentFiles[i].isDirectory()) {
+            obj[currentFiles[i].name] = mapSizes(
+                path + "/" + currentFiles[i].name
+            );
+        }
+    }
+    return obj;
+}
+
+fs.writeFileSync(
+    __dirname + "/fun-with-fs/" + "/myFiles.json",
+    JSON.stringify(x, null, 4)
+);
